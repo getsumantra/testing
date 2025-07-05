5e7cb7de194f71b4338628c6ae8b7c95180878a5
unknown <primes461@gmail.com>
2025-07-05 18:17:23 +0530

    pip_added

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ resource "azurerm_resource_group" "rg" {
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "stg" {
-  name                     = "teststorageacct"
-  resource_group_name      = "test-rg"
-  location                 = "West Europe"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_public_ip" "pip" {
+  name = "test-pip"
+  resource_group_name = "test-rg"
+  location = "West Europe"
+  allocation_method = "Static"
+  sku = "Standard"
 }
