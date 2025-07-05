52875f8b7ee9490cac972aae97fea2cbc791c834
unknown <primes461@gmail.com>
2025-07-05 18:21:31 +0530

    e

--- main.tf
+++ main.tf
@@ -3,10 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = "West Europe"
 }
 
-resource "azurerm_public_ip" "pip" {
-  name = "test-pip"
-  resource_group_name = "test-rg"
-  location = "West Europe"
-  allocation_method = "Static"
-  sku = "Standard"
+resource "azurerm_resource_group" "rg02" {
+  name = "test-rg02"
+  location = "west europe"
 }
