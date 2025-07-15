ea7d0970733d5eade9f7e819597b4153a7e379e1
unknown <primes461@gmail.com>
2025-07-15 23:42:11 +0530

    provider

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.36.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = "45d2c8fb-faba-4dc1-ab43-5ad9451eef1c"
+}
